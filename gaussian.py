4a4932aa2e419f132b4b1d80a3c011b990dfac2a
Isa Phuyuthanon <isaphu>
2021-08-03 20:24:21 +0700

    update

--- gaussian.py
+++ gaussian.py
@@ -1,25 +1,26 @@
 import math
 import matplotlib.pyplot as plt
 
+
 class Gaussian():
-    def __init__(self, mu = 0, sigma = 1):
+    def __init__(self, mu=0, sigma=1):
         self.mean = mu
         self.stdev = sigma
         self.data = []
-    
+
     def calculate_mean(self):
 
-        avg = 1.0* sum(self.data) /len(self.data)
+        avg = 1.0 * sum(self.data) / len(self.data)
 
         self.mean = avg
 
         return self.mean
 
-    def calculate_stdev (self, sample=True):
+    def calculate_stdev(self, sample=True):
 
         if sample:
             n = len(self.data) - 1
-        else: 
+        else:
             n = len(self.data)
 
         mean = self.mean
@@ -28,13 +29,13 @@ class Gaussian():
 
         for d in self.data:
             sigma += (d - mean) ** 2
-        
+
         sigma = math.sqrt(sigma / n)
 
-        self.stdev = sigma 
+        self.stdev = sigma
 
         return self.stdev
-    
+
     def read_data_file(self, file_name, sample=True):
 
         with open(file_name) as file:
@@ -55,13 +56,12 @@ class Gaussian():
         plt.xlabel('data')
         plt.ylabel('count')
 
-    def pdf(self,x):
+    def pdf(self, x):
         return (1.0/(self.stdev * math.sqrt(2*math.pi))) * math.exp(-0.5*((X - self.mean) / self.stdev) ** 2)
 
-    def plot_histogram_pdf(self, n_spaces = 50):
+    def plot_histogram_pdf(self, n_spaces=50):
 
-
-        mu = self.mean 
+        mu = self.mean
         sigma = self.stdev
 
         min_range = min(self.data)
@@ -75,6 +75,16 @@ class Gaussian():
             tmp = min_range + interval * i
             x.append(tmp)
             y.append(self.pdf(tmp))
-        
-        
 
+        fig, axes = plt.subplots(2,sharex=True)
+        fig.subplots_adjust(hspace=.5)
+        axes[0].hist(self.data, density=True)
+        axes[0].set_title('Normed Histogram of Data')
+        axes[0].set_ylabel('Density')
+
+        axes[1].plot(x,y)
+        axes[1].set_title('Normal Distribution for \n Sample mean and Sample Standard Devieation')
+        axes[0].set_ylabel('Density')
+        plt.show()
+
+        return x,y

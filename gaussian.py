542983503dcc8951fc70c90cd120bc7ab4513466
Isa Phuyuthanon <isaphu>
2021-08-04 13:36:22 +0700

    adding magic method to Gaussain

--- gaussian.py
+++ gaussian.py
@@ -88,3 +88,18 @@ class Gaussian():
         plt.show()
 
         return x,y
+
+        #adding magic method
+
+    def __add__ (self, other):
+        
+        result = Gaussian()
+        result.mean = self.mean + other.mean
+        result.stdev = math.sqrt(self.stdev ** 2 + other.stdev ** 2)
+
+        return result
+    def __repr__(self):
+
+        return "mean {}, standard deviation {}".format(self.mean, self.stdev)
+        
+

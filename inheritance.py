3247679d94b452477372d729f1e432f139e5e9d2
isaphu <uronohan@gmail.com>
2021-08-05 10:35:40 +0700

    inheritance complete

--- inheritance.py
+++ inheritance.py
@@ -19,4 +19,16 @@ class Shirt(Clothing):
     def __init__(self, color, size, style, price, long_or_short):
         Clothing.__init__(self, color, size, style, price)
         self.long_or_short = long_or_short
-        
+
+    def double_price(self):
+        self.price = 2 * self.price
+
+class Pants(Clothing):
+
+    def __init__(self, color, size, style, price, waist):
+        Clothing.__init__(self, color, size, style,price)
+        self.waist = waist
+
+    def calculate_discount(self, discount):
+        return self.price * (1 - discount / 2)
+

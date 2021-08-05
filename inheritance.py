c90c529d4908eafa1af550237aa40c75eb810597
isaphu <uronohan@gmail.com>
2021-08-05 10:32:30 +0700

    adding inheritance

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+#inheritance 
+
+class Clothing:
+
+    def __init__(self, color, size, style, price):
+        self.color = color
+        self.size = size 
+        self.style = style
+        self.price = price
+    
+    def change_price(self, price):
+        self.price = price
+
+    def calculate_discount(self, discount):
+        return self.price * (1 - discount)
+
+class Shirt(Clothing):
+
+    def __init__(self, color, size, style, price, long_or_short):
+        Clothing.__init__(self, color, size, style, price)
+        self.long_or_short = long_or_short
+        

e6013ce0451d61d034dda6ed2949e1f384300875
isaphu <uronohan@gmail.com>
2021-08-05 10:52:56 +0700

    adding inheritance in Gaussian class

--- gaussian.py
+++ gaussian.py
@@ -1,12 +1,31 @@
 import math
 import matplotlib.pyplot as plt
 
+#inheritance 
+
+class Distribution:
+
+    def __init__(self, mu=0, sigma=1):
+
+        self.mean = mu 
+        self.stdev = sigma
+        self.data = []
+
+    def read_data_file(self, file_name):
+
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+            file.close()
+
+            self.data = data_list
 
 class Gaussian():
     def __init__(self, mu=0, sigma=1):
-        self.mean = mu
-        self.stdev = sigma
-        self.data = []
+        Distribution.__init__(self,mu,sigma)
 
     def calculate_mean(self):
 
